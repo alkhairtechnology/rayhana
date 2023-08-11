94d223e275fd90432071c8bd9c3f1d7566018e61
anuar19 <50532945+anuar19@users.noreply.github.com>
2023-08-12 02:04:19 +0800

    Hide the signup link from login.tsx footer

--- apps/web/pages/auth/login.tsx
+++ apps/web/pages/auth/login.tsx
@@ -90,13 +90,13 @@ export default function Login({
   const safeCallbackUrl = getSafeRedirectUrl(callbackUrl);
 
   callbackUrl = safeCallbackUrl || "";
-
+/** 
   const LoginFooter = (
     <a href={`${WEBSITE_URL}/signup`} className="text-brand-500 font-medium">
       {t("dont_have_an_account")}
     </a>
   );
-
+*/
   const TwoFactorFooter = (
     <Button
       onClick={() => {
