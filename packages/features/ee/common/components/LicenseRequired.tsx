483266ffa4d94c6c2a3c71c11b680507c3677529
anuar19 <50532945+anuar19@users.noreply.github.com>
2023-08-12 02:17:14 +0800

    Initial setup for Takwim

--- packages/features/ee/common/components/LicenseRequired.tsx
+++ packages/features/ee/common/components/LicenseRequired.tsx
@@ -17,76 +17,20 @@ type LicenseRequiredProps = {
 };
 
 const LicenseRequired = ({ children, as = "", ...rest }: LicenseRequiredProps) => {
-  const session = useSession();
   const { t } = useLocale();
   const Component = as || Fragment;
-  const hasValidLicense = session.data ? session.data.hasValidLicense : null;
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === "development" && hasValidLicense === false) {
-      // Very few people will see this, so we don't need to translate it
-      console.info(
-        `You're using a feature that requires a valid license. Please go to ${WEBAPP_URL}/auth/setup to enter a license key.`
-      );
-    }
-  }, []);
 
   return (
     <Component {...rest}>
-      {hasValidLicense === null || hasValidLicense ? (
-        children
-      ) : process.env.NODE_ENV === "development" ? (
-        /** We only show a warning in development mode, but allow the feature to be displayed for development/testing purposes */
-        <>
-          <TopBanner
-            text=""
-            actions={
-              <>
-                {t("enterprise_license")}.{" "}
-                <Trans i18nKey="enterprise_license_development">
-                  You can test this feature on development mode. For production usage please have an
-                  administrator go to{" "}
-                  <a href={`${WEBAPP_URL}/auth/setup`} className="underline">
-                    /auth/setup
-                  </a>{" "}
-                  to enter a license key.
-                </Trans>
-              </>
-            }
-            variant="warning"
-          />
-          {children}
-        </>
-      ) : (
-        <EmptyScreen
-          Icon={AlertTriangle}
-          headline={t("enterprise_license")}
-          description={
-            <div
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(
-                  t("enterprise_license_description", {
-                    consoleUrl: `<a href="${CONSOLE_URL}" target="_blank" rel="noopener noreferrer" class="underline">
-                ${APP_NAME}
-              </a>`,
-                    setupUrl: `<a href="${WEBAPP_URL}/auth/setup" class="underline">/auth/setup</a>`,
-                    supportMail: `<a href="mailto:${SUPPORT_MAIL_ADDRESS}" class="underline">
-                ${SUPPORT_MAIL_ADDRESS}</a>`,
-                  })
-                ),
-              }}
-            />
-          }
-        />
-      )}
+      {children}
     </Component>
   );
 };
 
 export const withLicenseRequired =
   <T extends JSX.IntrinsicAttributes>(Component: ComponentType<T>) =>
-  // eslint-disable-next-line react/display-name
-  (hocProps: T) =>
+    // eslint-disable-next-line react/display-name
+    (hocProps: T) =>
     (
       <LicenseRequired>
         <Component {...hocProps} />
